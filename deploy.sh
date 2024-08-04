4011368de763ef89674da820603f4841da7fe671
Joel Walsh-West <joelwalshwest@gmail.com>
2024-08-04 17:08:45 -0400

    Adds project id to the deployment command

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build -t joelwalshwest/fastapi-scaffolding . --target prod_app --platform=linux/amd64
 docker push joelwalshwest/fastapi-scaffolding
-gcloud run deploy fastapi-scaffolding --image joelwalshwest/fastapi-scaffolding  --platform managed  --allow-unauthenticated
+gcloud run deploy fastapi-scaffolding --image joelwalshwest/fastapi-scaffolding  --platform managed  --allow-unauthenticated --project fastapi-scaffolding
