bd1adb0121fd8212faaa269e0ab2ed7669f96e19
= <joelwalshwest@gmail.com>
2024-08-08 04:42:35 +0000

    Adds support for entering into a running container

--- dev.sh
+++ dev.sh
@@ -1,2 +1,2 @@
 docker build -t fastapi-scaffolding-image . --target dev_app    
-docker run -it -p 80:80 -p 5678:5678 -v $(pwd):/code fastapi-scaffolding-image 
+docker run -it --rm --name fastapi-scaffolding-image-container -p 80:80 -p 5678:5678 -v $(pwd):/code fastapi-scaffolding-image 
--- run-image.sh
+++ run-image.sh
@@ -1,2 +1,2 @@
 docker build -t fastapi-scaffolding-image . --target prod_app    
-docker run -it -p 80:80 -p 5678:5678 -v $(pwd):/code fastapi-scaffolding-image 
+docker run --name fastapi-scaffolding-image-container --rm -it -p 80:80 -p 5678:5678 -v $(pwd):/code fastapi-scaffolding-image 
