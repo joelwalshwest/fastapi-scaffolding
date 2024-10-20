d1c1b80f0dd4aab6183a9f1f378335882d2e3cb2
Joel Walsh-West <joelwalshwest@gmail.com>
2024-10-20 16:37:18 -0400

    Moves allowed CORS origins into a constants module

--- src/main.py
+++ src/main.py
@@ -1,26 +1,19 @@
 from fastapi import FastAPI
 from src.endpoints.debug import health_check
 from src.utils import environment
-from src.utils.environment import Environment
+from src.utils import constants
 from fastapi.middleware.cors import CORSMiddleware
 
-if environment.Environment.current() == Environment.LOCAL:
+if environment.Environment.current() == environment.Environment.LOCAL:
     import debugpy
 
     debugpy.listen(("127.0.0.1", 5678))
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:80",
-    "https://qa.react-scaffolding.joelwalshwest.com",
-    "https://react-scaffolding.joelwalshwest.com",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=constants.ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,4 +24,4 @@ app.include_router(health_check.router)
 
 @app.get("/")
 async def root():
-    return "I'm just here showing off my automated builds!"
+    return {}
